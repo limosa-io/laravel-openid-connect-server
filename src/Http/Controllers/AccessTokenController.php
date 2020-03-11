6aa6c9c956152de38d73bbb817b5e2b593df875f
Arie Timmerman <arietimmerman@gmail.com>
2020-03-11 22:22:36 +0100

    two error handling workarounds

--- src/Http/Controllers/AccessTokenController.php
+++ src/Http/Controllers/AccessTokenController.php
@@ -1,27 +1,35 @@
 <?php
 
-/**
- * Not needed as soon as https://github.com/thephpleague/oauth2-server/pull/1082 is used. 
- */
-
 namespace Idaas\Passport\Http\Controllers;
 
+use Idaas\Passport\Passport;
 use Laminas\Diactoros\Response as Psr7Response;
 use Laravel\Passport\Http\Controllers\AccessTokenController as ControllersAccessTokenController;
 use Laravel\Passport\Exceptions\OAuthServerException;
+use League\OAuth2\Server\CryptTrait;
 use League\OAuth2\Server\Exception\OAuthServerException as LeagueException;
 
 class AccessTokenController extends ControllersAccessTokenController
 {
+
+    use CryptTrait;
 
     protected function withErrorHandling($callback)
     {
         try {
             return $callback();
         } catch (LeagueException $e) {
-
             if ($e->getErrorType() == 'invalid_request' && $e->getHint() == 'Authorization code has been revoked') {
+
+                // TOOD: Not needed as soon as https://github.com/thephpleague/oauth2-server/pull/1082 is used.
                 $e = LeagueException::invalidGrant($e->getHint());
+
+                // TODO: This is an ugly workaround to revoke an earlier issued access token if an authorization code
+                // is used twice
+                $encryptedAuthCode = $_POST['code'];
+                $this->setEncryptionKey(app('encrypter')->getKey());
+                $authCodePayload = \json_decode($this->decrypt($encryptedAuthCode));
+                Passport::token()->where('user_id', $authCodePayload->user_id)->update(['revoked' => true]);
             }
 
             throw new OAuthServerException(
