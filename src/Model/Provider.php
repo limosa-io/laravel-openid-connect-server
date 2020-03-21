dccafeb3752d40ff04d5b03eb33b37ba9cdf63b3
Arie Timmerman <arietimmerman@gmail.com>
2020-03-21 20:26:01 +0100

    default provider

--- src/Model/Provider.php
+++ src/Model/Provider.php
@@ -4,10 +4,84 @@ namespace Idaas\Passport\Model;
 
 class Provider implements ProviderInterface
 {
+
+    protected $wellKnown;
+
+    public function __construct()
+    {
+        $this->wellKnown = array(
+            'response_types_supported' =>
+            [
+                'code',
+                'token',
+                'id_token',
+                'code token',
+                'id_token token',
+            ],
+            'acr_values_supported' =>
+            [
+                'urn:mace:incommon:iap:gold',
+                'urn:mace:incommon:iap:silver',
+                'urn:mace:incommon:iap:bronze',
+            ],
+            'scopes_supported' =>
+            [
+                'openid',
+                'online_access',
+                'profile',
+                'email',
+                'address',
+                'phone',
+                'profile',
+                'email',
+                'address',
+                'phone',
+                'roles'
+            ],
+            'authorization_endpoint' => route('oauth.authorize', []),
+            'token_endpoint' => route('oauth.token', []),
+            'userinfo_endpoint' => route('oidc.userinfo', []),
+            'jwks_uri' => route('oidc.jwks', []),
+            'issuer' => url('/'),
+            'claims_supported' =>
+            [
+                'sub',
+                'iss',
+                'roles',
+                'acr',
+                'picture',
+                'profile',
+            ],
+            'end_session_endpoint' => route('oidc.logout', []),
+            'code_challenge_methods_supported' =>
+            [
+                'S256',
+            ],
+            'introspection_endpoint' => route('oauth.introspect'),
+            'introspection_endpoint_auth_methods_supported' =>
+            [
+                'client_secret_jwt',
+            ],
+            'token_endpoint_auth_methods_supported' =>
+            [
+                'none',
+                'client_secret_post',
+                'client_secret_basic',
+            ],
+            'revocation_endpoint' => route('oauth.revoke'),
+            'service_documentation' => url('/'),
+            'ui_locales_supported' =>
+            [
+                'en-GB',
+                'nl-NL',
+            ],
+        );
+    }
+
     public function toJson($options = 0)
     {
         return json_encode([
-            // attributes
+            $this->wellKnown
         ], $options);
     }
 }
