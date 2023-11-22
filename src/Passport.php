58b368fa3e87b79fa187de16086539d2422fad2c
Oluwatobi Omisakin <oluwatobi.omisakin@eideasy.com>
2023-11-22 18:09:42 +0200

    feat: Register other routes

--- src/Passport.php
+++ src/Passport.php
@@ -21,5 +21,30 @@ class Passport extends LaravelPassport
      */
     public static function routes($callback = null, array $options = [])
     {
+        $registerWellKnown = $callback == null;
+        $callback = $callback ?: function ($router) {
+
+        };
+
+        $defaultOptions = [
+            'prefix' => 'oauth',
+            'namespace' => '\Laravel\Passport\Http\Controllers',
+        ];
+
+        $options = array_merge($defaultOptions, $options);
+
+        Route::group($options, function ($router) use ($callback) {
+            $callback(new RouteRegistrar($router));
+        });
+
+        // The wellKnown endpoints must be registered without a prefix
+        if ($registerWellKnown) {
+            Route::group([
+                'namespace' => '\Laravel\Passport\Http\Controllers'
+            ], function ($router) use ($callback) {
+                $router = new RouteRegistrar($router);
+                $router->forWellKnown();
+            });
+        }
     }
 }
--- src/PassportServiceProvider.php
+++ src/PassportServiceProvider.php
@@ -63,6 +63,19 @@ class PassportServiceProvider extends LaravelPassportServiceProvider
         ], function ($router) {
             $router = new RouteRegistrar($router);
             $router->forWellKnown();
+            Route::group([
+                'prefix' => 'oauth',
+                'namespace' => '\Laravel\Passport\Http\Controllers',
+            ], function ($router) {
+                $router = new RouteRegistrar($router);
+                $router->forAuthorization();
+                $router->forAccessTokens();
+                $router->forIntrospect();
+                $router->forUserinfo();
+                $router->forManagement();
+                $router->forOIDCClients();
+            });
+
         });
 
         parent::registerRoutes();
